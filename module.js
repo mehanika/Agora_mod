c96db19f1a622960c8c07aaf22a8c4c97905d9fa
mehanika <mehanika91@gmail.com>
2012-10-11 21:33:45 -0500

    module.js sin errores de sintaxis

--- module.js
+++ module.js
@@ -1,34 +1,11 @@
 YUI().use("io", "json-parse", "node", 'json-stringify',
     function(Y) {
-	
-       
 
+        //Data fetched will be displayed in a UL in the
+        //element #output:
+       /// var output = Y.one("#output ul");
 
-        var handleStart = function(id, a) {
-            Y.log("iniciando busqueda");
-            //output.set("innerHTML", "<li>Loading news stories via Yahoo! Pipes feed...</li>");
-        }
-
-	function mostrarResultadoBusqueda(recursos)
-	{
-		var resultado_busqueda = Y.one("#resultado_busqueda");
-		var text='<table>';
-			for(recurso in recursos) {
-
-				if (recursos[recurso].titulo != '') {
-					text += '<tr>';
-					text += '<td><img src="' + recursos[recurso].icono + '" title="' + recursos[recurso].extension + '" width="16" height="16" border="0" /></td>';
-					text += '<td><a href="' + recursos[recurso].url_base + 'recurso/ver/contenido/' + recursos[recurso].id_recurso + '" title="' + recursos[recurso].comentario + '. ' + recursos[recurso].descripcion + '">' + recursos[recurso].titulo + '</a></td>';
-					text += '</tr>';
-				}
-			}
-			text += '</table>';
-
-          Y.log("exito en en recibir la respuesta");
-	resultado_busqueda.set("innerHTML", text);
-	}
-
-	
+        //Event handler called when the transaction begins:
 	function obtenerFuncionVisualizacionDoumentos(extensionArchivo,url,titulo)
 	{
 		var funcionVisualizacion = "";
@@ -36,23 +13,71 @@ YUI().use("io", "json-parse", "node", 'json-stringify',
 		var urlGdocsViewer = 'http://docs.google.com/viewer?url='+urlDoc+'&embedded=true';
 		if(extensionArchivo == "ppt" || extensionArchivo == "pdf" || extensionArchivo == "doc" || extensionArchivo == "odt" || 			extensionArchivo == "xls")
 		{
-			funcionVisualizacion = "onclick=visualizar('"+titulo+"','"+urlGdocsViewer+"');"
+			funcionVisualizacion = "onclick=visualizar('"+titulo+"','"+urlGdocsViewer+"') return false;"
 		}
 
 		return funcionVisualizacion;
 	}
-   
+	
+	function openbox(_url, fadin)
+	{
+	  var box = document.getElementById('box'); 
+	  document.getElementById('shadowing').style.display='block';
+
+	  var btitle = document.getElementById('boxtitle');
+	  btitle.innerHTML = "Calidad de los objetos de aprendizaje";
+	  var url = encodeURIComponent(_url);
+	  var newUrl = 'http://docs.google.com/viewer?url='+url+'&embedded=true';
+	 var iframe = '<iframe src="'+newUrl+'" width="100%" height="100%" style="border: none;"></iframe>'
+	var content = document.getElementById('boxcontent');
+	var gframe  = document.getElementById('gframe');
+	gframe.src= newUrl;
+	//content.innerHTML = iframe;
+
+	  if(fadin)
+	  {
+		 gradient("box", 0);
+		 fadein("box");
+	  }
+	  else
+	  { 	
+	    box.style.display='block';
+	  }  	
+	}
+	
+	
+	
+	
+        var handleStart = function(id, a) {
+            Y.log("iniciando busqueda");
+            //output.set("innerHTML", "<li>Loading news stories via Yahoo! Pipes feed...</li>");
+        }
+
+        //Event handler for the success event -- use this handler to write the fetched
+        //RSS items to the page.
         var handleSuccess = function(id, o, a) {
 
             //We use JSON.parse to sanitize the JSON (as opposed to simply performing an
             //JavaScript eval of the data):
 		var resultado_busqueda = Y.one("#resultado_busqueda");
-            	var recursos = Y.JSON.parse(o.responseText);
-		
-		mostrarResultadoBusqueda(recursos);
+            var recursos = Y.JSON.parse(o.responseText);
 
           
+var text='<table>';
+			for(recurso in recursos) {
 
+				if (recursos[recurso].titulo != '') {
+					text += '<tr>';
+					text += '<td><img src="' + recursos[recurso].icono + '" title="' + recursos[recurso].extension + '" width="16" height="16" border="0" /></td>';
+					text += '<td><a href="' + recursos[recurso].url_base + 'recurso/ver/contenido/' + recursos[recurso].id_recurso + '" title="' + recursos[recurso].comentario + '. ' + recursos[recurso].descripcion + '" '+'>' + recursos[recurso].titulo + '</a></td>';
+					text += '</tr>';
+				}
+			}
+			text += '</table>';
+
+          Y.log("exito en en recibir la respuesta");
+	resultado_busqueda.set("innerHTML", text);
+        }
 
         //In the event that the HTTP status returned does not resolve to,
         //HTTP 2xx, a failure is reported and this function is called:
@@ -75,27 +100,24 @@ YUI().use("io", "json-parse", "node", 'json-stringify',
         };
 
       
-	
-	var url = "http://localhost/moodle/mod/agora/proxy_s.php?url=http://sel.uady.mx/agora/recurso/ajax/buscar/?cadena="
-	
+
+	var url = "http://localhost/moodle/mod/agora/proxy_s.php?url=http://smile.esi.uclm.es/agora/recurso/ajax/buscar/?cadena="
+
        
 	 var handleClick = function(o) {
 		var palabraBuscar = Y.one("#campo_busqueda").get("value");
-		var palabraBuscar = escape(palabraBuscar);
+		palabraBuscar = escape(palabraBuscar);
 		var urlBusqueda = url+palabraBuscar;
             Y.log("Click detected; beginning io request to Yahoo! Pipes.", "info", "example");
 	    // Remove the default "X-Requested-With" header as this will
 	    // prevent the request from succeeding; the Pipes
 	    // resource will not accept user-defined HTTP headers.
 	  Y.io.header('X-Requested-With');
-            var obj = Y.io(
-                //this is a specific Pipes feed, populated with cycling news:
-               urlBusqueda,
-                cfg
-            );
+            var obj = Y.io(urlBusqueda,cfg);
         }
 
         //add the click handler to the Load button.
         Y.on("click", handleClick, "#boton_buscar");
+  
     }
 );
